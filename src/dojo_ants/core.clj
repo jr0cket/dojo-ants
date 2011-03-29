efd6b531650b194767071c7f8cf4fe00e58f1bf2
Dale Thatcher <github@dalethatcher.com>
2011-03-29 22:00:52 +0100

    stuff

--- src/dojo_ants/core.clj
+++ src/dojo_ants/core.clj
@@ -1,1 +1,26 @@
+
 (ns dojo-ants.core)
+
+(defn create-map
+  "Create two-dimensional vector for world completely covered by fog of war"
+  [x y]
+  (vec (repeat y (vec (repeat x :unknown)))))
+
+(def command-map {"a" (fn [_ owner] (if (= "0" owner) :friend :foe)) "f" (constantly :food) "w" (constantly :water)} )
+
+(defn parse-current-world
+  "Apply command and arguments to specified world"
+  [world command]
+  (let [[type x y & owner] (seq (.split command " "))]
+    (apply update-in world [(Integer/parseInt y) (Integer/parseInt x)] (command-map type) owner)))
+
+(defn is-item-here? [item x y world]
+  (= item (get-in world [y x])))
+
+(defn is-ant-here? [x y world] (or (is-item-here? :friend x y world) (is-item-here? :foe x y world)))
+(def is-food-here? (partial is-item-here? :food))
+(def is-friend-here? (partial is-item-here? :friend))
+(def is-foe-here? (partial is-item-here? :foe))
+(def is-water-here? (partial is-item-here? :water))
+
+  
--- test/dojo_ants/test/core.clj
+++ test/dojo_ants/test/core.clj
@@ -2,5 +2,26 @@
   (:use [dojo-ants.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest create-map-returns-correct-empty-map
+  (is (= 10 (count (create-map 20 10))))
+  (is (= 20 (count (first (create-map 20 10))))))
+
+(deftest parse-ant-location
+  (is (is-ant-here? 0 0 (parse-current-world [ [nil] ] "a 0 0 0")))
+  (is (is-friend-here? 0 0 (parse-current-world [ [nil] ] "a 0 0 0")))
+  (is (is-foe-here? 0 0 (parse-current-world [ [nil] ] "a 0 0 1")))
+  (is (is-food-here? 0 0 (parse-current-world [[nil]] "f 0 0"))))
+
+(deftest parse-ant-with-real-location
+  (is (is-ant-here? 0 0 (parse-current-world [ [nil] ] "a 0 0 0")))
+  (is (is-friend-here? 1 2 (parse-current-world (create-map 4 4) "a 1 2 0")))
+  (is (is-foe-here? 2 3 (parse-current-world (create-map 4 4) "a 2 3 1")))
+  (is (is-food-here? 3 4 (parse-current-world (create-map 6 6) "f 3 4")))
+  (is (is-water-here? 4 5 (parse-current-world (create-map 6 6 ) "w 4 5"))))
+
+(deftest parse-multiple-commands
+  (let [first-ant-world (parse-current-world (create-map 5 5) "a 1 2 0")
+        ant-plus-food (parse-current-world first-ant-world "f 3 4")]
+    (is (is-friend-here? 1 2 ant-plus-food))
+    (is (is-food-here? 3 4 ant-plus-food)))
+  )
