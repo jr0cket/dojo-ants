61a5a4bf1da5e163a8864343eebf088bb5f4c68f
John Stevenson <john@jr0cket.com>
2011-03-31 15:02:19 +0100

    Using autodoc 0.8.0-SNAPSHOT to remove dependency on earlier clojure version, plus README updates

--- project.clj
+++ project.clj
@@ -2,6 +2,16 @@
   :description "A representation of the ants world, able to parse commands"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
-  :dev-dependencies [[autodoc "0.7.1"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]]
+  :autodoc { :name "London Clojure dojo - Group 5", :page-title "Ants dojo Documentation"}
+
+  ;; Only re-fetch dependencies when they change in project.clj or
+  ;; when :library-path directory is empty.
+  :checksum-deps true  
+  
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   )
